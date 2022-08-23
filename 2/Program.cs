d9465d731c1268a7083f2d9969c3661546b1a852
Ondra Janoška <o.janoska98@gmail.com>
2022-08-23 17:49:01 +0200

    2. uloha; nevedel jsem zadani lol, to mozna bude problem

---
+++ 2/Program.cs
@@ -0,0 +1,43 @@
+﻿//Console.WriteLine("Ohayoo C: uwu whats ur file name?");
+
+//var filename = Console.ReadLine();
+string filename = "file.txt";
+if (filename is null) {
+    Console.WriteLine("Error: filename is null! What the hell did you input?");
+}
+
+StreamReader reader = new StreamReader(filename);
+
+
+string wordICareAbout = "";
+char[] delimiters = {'.', ',', ' ', '\n', '\t'};
+
+Dictionary<string, int> cetnosti = new Dictionary<string, int>();
+
+while (true) {
+    Int32 readValue = reader.Read();
+    if (readValue == -1){
+        break;
+    }
+    char chr = (char)readValue;
+    if (delimiters.Contains(chr)){
+        try {
+            cetnosti[wordICareAbout]++;
+        }
+        catch (KeyNotFoundException){
+            if (wordICareAbout != ""){
+                cetnosti.Add(wordICareAbout, 1);
+            }
+        }
+        // add it to the dick
+        wordICareAbout = "";
+        continue;
+    }
+    wordICareAbout+=chr;
+}
+
+var orderedCetnosti = cetnosti.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+foreach(KeyValuePair<string, int> word in orderedCetnosti){
+    Console.WriteLine("{0} \t\t {1}", word.Key, word.Value);
+}
