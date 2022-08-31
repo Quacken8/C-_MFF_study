599ccc4575fa96492407d2258cd75ef72feb59f9
Ondra Janoška <o.janoska98@gmail.com>
2022-08-31 11:35:23 +0200

    actually tests init

---
+++ 6/6_tests/UnitTest1.cs
@@ -0,0 +1,23 @@
+namespace _6_tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void Input_WorksByteByByte()
+    {
+        /*
+        // Arange
+        var mockupByteInput[]{bajtybajtybajtybajty};
+        InputHandler ih = new InputHandler();
+
+        // Act
+        readBytes = ih.read(mockupByteInput);
+        
+
+        // Assert
+
+        Assert.AreEqual([bajtyy], readBytes);
+        */
+    }
+}
