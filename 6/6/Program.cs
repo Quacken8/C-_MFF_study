f1beaa95fb838fd310c874d524bfae68c14b5409
Ondra Janoška <o.janoska98@gmail.com>
2022-09-03 16:50:47 +0200

    well fuck me i guess, jak se sakra správně visiblují internaly?

--- 6/6/Program.cs
+++ 6/6/Program.cs
@@ -1,6 +1,100 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿[assembly:System.Runtime.CompilerServices.InternalsVisibleTo("UnitTest1")]
 
-public class Hello{
-    
+Console.WriteLine("Hello");
+
+bool fileEnd = false;
+
+while (!fileEnd){
+    //byte readByte = IByteReader.ReadByte();
+}
+/*
+interface IByteReader {
+    ///reads the next byte in file and returns it as int or returns null if end of file
+    byte ReadByte();
+}
+
+byte readByte(){
+    var reader = new StreamReader(filename);
+}
+*/
+
+/*
+Node combineNodes(Node leftNode, Node rightNode){
+    if (leftNode.combined || rightNode.combined){
+        throw new ArgumentException("One of the nodes was already combined before!");
+    }
+    leftNode.combined = true;
+    rightNode.combined = true;
+    Node newNode = new Node(leftNode.value+rightNode.value);
+    return newNode;
+}
+*/
+
+public class Tree{
+    List<TreeLayer> layers = new List<TreeLayer>();
+
+    void addLayer(TreeLayer layer){
+        /// adds a layer into the tree such that the lowest of the node values is higher than of all the nodes below
+        /// since this happens only when combining nodes and creating a new node of new highest value, no need to care about
+        /// sorting --- new layer is always on top
+        layers.Add(layer);
+    }
+
+    public void addNode(Node node){
+        /// adds a node to one of tree's layer such that the new node will be in the highest layer that doesnt have higher value in it
+        TreeLayer? lastLayer = null;
+        bool found = false;
+        foreach (TreeLayer layer in layers){
+            if (layer.lowestValue > node.value){
+                found = true;
+                try {
+                    lastLayer.addNode(node);
+                }
+                catch (NullReferenceException){
+                    throw new Exception("You tried to add a node that would be below the lowest layer of the tree. You should go through the nodes from lowest values up!");
+                }
+                
+                break;
+            }
+            lastLayer = layer;
+        }
+        if (!found) {   // oh so the new node is for a brand new top layer
+            TreeLayer newHighestLayer = new TreeLayer();
+            newHighestLayer.addNode(node);
+            this.addLayer(newHighestLayer);
+        }
+    }
+}
+
+public class TreeLayer{
+    List<Node> nodes = new List<Node>();
+    public int highestValue = int.MinValue;
+    public int lowestValue = int.MaxValue;
+    public void addNode(Node newNode){
+        nodes.Add(newNode);
+        if (newNode.value > highestValue){
+            highestValue = newNode.value;
+        }
+        if (newNode.value < lowestValue){
+            lowestValue = newNode.value;
+        }
+    }
+
+    void printTree(){
+
+    }
+}
+
+public class Node{
+    public int value;
+    public bool combined = false;
+    public Node(int value){
+        this.value = value;
+    }
+}
+
+public class Leaf : Node{
+    public Leaf(int value):base(value){
+        this.value = value;
+    }
 }
--- 6/6_tests/UnitTest1.cs
+++ 6/6_tests/UnitTest1.cs
@@ -1,8 +1,25 @@
 namespace _6_tests;
 
+
 [TestClass]
 public class UnitTest1
 {
+    [TestMethod]
+    public void TreeAdd_tryRegularAddingOfOkBytes(){
+        // Arange
+        Tree tree = new Tree();
+        byte[] mockupBytesToAdd = new byte[]{1, 2, 2, 10, 1, 3};
+
+        // Act
+        foreach (byte b in mockupBytesToAdd){
+            Node newnode = new Node(b);
+            tree.addNode(newnode);
+        }
+
+        // Assert
+        Assert.AreEqual(tree.layers, 3);
+    }
+
     [TestMethod]
     public void Input_WorksByteByByte()
     {
@@ -19,5 +36,15 @@ public class UnitTest1
 
         Assert.AreEqual([bajtyy], readBytes);
         */
+    }
+
+    class ByteReaderMockup: IByteReader {
+        public List<Byte> inputBytes = new List<byte>();
+
+        public int ReadByte(){
+            int toReturn = inputBytes[0];
+            inputBytes.RemoveAt(0);
+            return toReturn;
+        }
     }
 }
