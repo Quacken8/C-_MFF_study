c837ae72fe139ec7be3168455f6ea120af138993
Ondra Janoška <o.janoska98@gmail.com>
2022-09-05 10:04:42 +0200

    new recursive approach

--- 6/6/Program.cs
+++ 6/6/Program.cs
@@ -1,4 +1,5 @@
-﻿[assembly:System.Runtime.CompilerServices.InternalsVisibleTo("UnitTest1")]
+﻿[assembly:System.Runtime.CompilerServices.InternalsVisibleTo("_6_tests")]   // why the fuck are you not working
+
 
 Console.WriteLine("Hello");
 
@@ -18,83 +19,92 @@ byte readByte(){
 }
 */
 
-/*
-Node combineNodes(Node leftNode, Node rightNode){
-    if (leftNode.combined || rightNode.combined){
-        throw new ArgumentException("One of the nodes was already combined before!");
-    }
-    leftNode.combined = true;
-    rightNode.combined = true;
-    Node newNode = new Node(leftNode.value+rightNode.value);
-    return newNode;
+
+bool isFirstNodeLeft(Node firstNode, Node secondNode){
+    /// determines if firstNode should be added to the left in a tree
+    bool? firstNodeIsLeft = null;
+        if (firstNode.weight < secondNode.weight){
+            firstNodeIsLeft = true;
+        }
+        else if (firstNode.weight > secondNode.weight){
+            firstNodeIsLeft = false;
+        }
+        else if (firstNode.leaf && !secondNode.leaf){
+            firstNodeIsLeft = true;
+        }
+        else if (!firstNode.leaf && secondNode.leaf){
+            firstNodeIsLeft = false;
+        }
+
 }
-*/
 
-public class Tree{
-    List<TreeLayer> layers = new List<TreeLayer>();
+public class Node {
+    /// Node in Huffman binary tree. Node's symbol is the piece of information it represents and its weight is the number of occurances of the information in input 
+    public int weight;
+    public byte? symbol = null;
+    public Node[]? childern = null;
+    public Node? parent = null;
+    public bool combined = false;
+    public bool leaf;
 
-    void addLayer(TreeLayer layer){
-        /// adds a layer into the tree such that the lowest of the node values is higher than of all the nodes below
-        /// since this happens only when combining nodes and creating a new node of new highest value, no need to care about
-        /// sorting --- new layer is always on top
-        layers.Add(layer);
+    public Node(int weight, byte symbol){
+        /// for leaf init
+        this.weight = weight;
+        this.leaf = true;
+        this.symbol = symbol;
+    }
+
+    public Node(Node leftNode, Node rightNode){
+        /// for nonleaf init from combined nodes
+        this.weight = leftNode.weight + rightNode.weight;
+
+        this.childern = new Node[2]{leftNode, rightNode};
+
+        leftNode.combined = true;
+        leftNode.parent = this;
+        
+        rightNode.parent = this;
+        rightNode.combined = true;
+
+        this.leaf = false;
+    }
+}
+
+
+public class Tree {
+    Node highestNode;
+
+    public Tree(Node highestNode){
+        this.highestNode = highestNode;
     }
 
     public void addNode(Node node){
-        /// adds a node to one of tree's layer such that the new node will be in the highest layer that doesnt have higher value in it
-        TreeLayer? lastLayer = null;
-        bool found = false;
-        foreach (TreeLayer layer in layers){
-            if (layer.lowestValue > node.value){
-                found = true;
-                try {
-                    lastLayer.addNode(node);
-                }
-                catch (NullReferenceException){
-                    throw new Exception("You tried to add a node that would be below the lowest layer of the tree. You should go through the nodes from lowest values up!");
-                }
-                
-                break;
+        if (isFirstNodeLeft(this.highestNode, node){
+            this.highestNode = new Node(this.highestNode, node);
+        }
+        else {
+            this.highestNode = new Node(node, this.highestNode);
+        }
+    }
+
+    public void printTree(){
+        List<Node> unexploredIntersections = new List<Node>{this.highestNode};
+ 
+        string toPrint = "";
+
+        Node? workingNode = null;
+
+        while(unexploredIntersections.Count != 0){
+            workingNode = unexploredIntersections.Last();
+            unexploredIntersections.RemoveAt(unexploredIntersections.Count - 1);
+            if (workingNode.leaf){
+                // TBD CHANGE TO STRINGG BUILDER THAT LIVES AROIND THE WHOLE WHILE LOOP!
+                toPrint += workingNode.symbol + ":" + workingNode.weight + " ";
             }
-            lastLayer = layer;
+            else {
+                unexploredIntersections.Add(workingNode.childern[1]);
+                unexploredIntersections.Add(workingNode.childern[0]);
+            }
         }
-        if (!found) {   // oh so the new node is for a brand new top layer
-            TreeLayer newHighestLayer = new TreeLayer();
-            newHighestLayer.addNode(node);
-            this.addLayer(newHighestLayer);
-        }
-    }
-}
-
-public class TreeLayer{
-    List<Node> nodes = new List<Node>();
-    public int highestValue = int.MinValue;
-    public int lowestValue = int.MaxValue;
-    public void addNode(Node newNode){
-        nodes.Add(newNode);
-        if (newNode.value > highestValue){
-            highestValue = newNode.value;
-        }
-        if (newNode.value < lowestValue){
-            lowestValue = newNode.value;
-        }
-    }
-
-    void printTree(){
-
-    }
-}
-
-public class Node{
-    public int value;
-    public bool combined = false;
-    public Node(int value){
-        this.value = value;
-    }
-}
-
-public class Leaf : Node{
-    public Leaf(int value):base(value){
-        this.value = value;
     }
 }
