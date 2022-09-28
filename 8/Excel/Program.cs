7549094895c6189179870782a3a31f32cd4dc397
Ondra Janoška <o.janoska98@gmail.com>
2022-09-28 20:04:27 +0200

    numbers to letters work now, working on the other way around

--- 8/Excel/Program.cs
+++ 8/Excel/Program.cs
@@ -14,22 +14,62 @@ public class Sheet {
 /// </summary>
 public static class AssortedExcelFunctions{
     /// <summary>
-    /// used to converting from column number to letter representation
+    /// used to converting from column number to letter representation indexing from 0
     /// </summary>
     /// <param name="number">The number of the column</param>
     /// <returns>column name in letter representation</returns>
     public static string numberToColumnName(int number){
-        const int padding = 65; // number after which in chars the alphabet starts
+        const int padding = 65; // number at which in chars the alphabet starts
         const int lettersInAlphabet = 26;
-        string name = "";
+        Stack<char> nameStack = new Stack<char>();
         do
         {
             int modulo = (number % lettersInAlphabet);
-            number -= modulo;
             number /= lettersInAlphabet;
-            name += (char)(modulo+padding);
-        } while (number > 0);
+            number--;
+            nameStack.Push((char)(modulo + padding));
 
+        } while (number >= 0);
+        string name = "";
+        
+        while (nameStack.Count > 0){
+            name += nameStack.Pop();
+        }
         return name;
+    }
+
+    /// <summary>
+    /// quickly-ish makes a baseInt**power
+    /// </summary>
+    /// <param name="baseInt"></param>
+    /// <param name="power"></param>
+    /// <returns></returns>
+    public static int intPow(int baseInt, int power){
+        int toReturn = 1;
+        while (power > 1){
+            toReturn *= baseInt;
+            power--;
+        }
+        return toReturn;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="name"></param>
+    /// <returns>number of column represented by 'name'</returns>
+    public static int columnNameToNumber(string name){
+        const int padding = 65; // number at which in chars the alphabet starts
+        const int lettersInAlphabet = 26;
+        int colNumber = 0;
+        char[] nameArr = name.ToCharArray();
+        for (int i = 0; i < nameArr.Length; i++)
+        {
+            int power = nameArr.Length - 1 - i;
+            int digit = (int)nameArr[i] - padding;
+            colNumber += digit * intPow(lettersInAlphabet, power);
+        }
+
+        return colNumber;
     }
 }
--- 8/tests/UnitTest1.cs
+++ 8/tests/UnitTest1.cs
@@ -26,15 +26,83 @@ public class UnitTest1
         Assert.AreEqual(expectedName, name);
     }
 
-        [TestMethod]
+    [TestMethod]
     public void numberToColumnName_test26AA()
     {
-        int number = 25;
+        int number = 26;
 
         string name = AssortedExcelFunctions.numberToColumnName(number);
         string expectedName = "AA";
         Console.WriteLine(name);
 
         Assert.AreEqual(expectedName, name);
+    }
+    [TestMethod]
+    public void numberToColumnName_test27AB()
+    {
+        int number = 27;
+
+        string name = AssortedExcelFunctions.numberToColumnName(number);
+        string expectedName = "AB";
+        Console.WriteLine(name);
+
+        Assert.AreEqual(expectedName, name);
+    }    
+    [TestMethod]
+    public void numberToColumnName_testAAA()
+    {
+        int number = 26+26*26;
+
+        string name = AssortedExcelFunctions.numberToColumnName(number);
+        string expectedName = "AAA";
+        Console.WriteLine(name);
+
+        Assert.AreEqual(expectedName, name);
+    }
+
+    [TestMethod]
+    public void columnNameToNumber_test0A(){
+        string name = "A";
+        int number = AssortedExcelFunctions.columnNameToNumber(name);
+
+        int expectedNumber = 0;
+
+        Console.WriteLine(number);
+
+        Assert.AreEqual(expectedNumber, number);
+    }
+
+    [TestMethod]
+    public void columnNameToNumber_test1B(){
+        string name = "B";
+        int number = AssortedExcelFunctions.columnNameToNumber(name);
+
+        int expectedNumber = 1;
+
+        Console.WriteLine(number);
+
+        Assert.AreEqual(expectedNumber, number);
+    }    
+    [TestMethod]
+    public void columnNameToNumber_test25Z(){
+        string name = "Z";
+        int number = AssortedExcelFunctions.columnNameToNumber(name);
+
+        int expectedNumber = 25;
+
+        Console.WriteLine(number);
+
+        Assert.AreEqual(expectedNumber, number);
+    }
+    [TestMethod]
+    public void columnNameToNumber_test26AA(){
+        string name = "AA";
+        int number = AssortedExcelFunctions.columnNameToNumber(name);
+
+        int expectedNumber = 26;
+
+        Console.WriteLine(number);
+
+        Assert.AreEqual(expectedNumber, number);
     }
 }
